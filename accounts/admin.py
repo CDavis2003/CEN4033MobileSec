fb1e7edc0e5c3a3dc76aabd88ffa70758dbff908
DonFro <ayden.frometa@gmail.com>
2025-04-16 21:28:09 -0400

    allowed admin to see users

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser, Credential
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Credential)
