3d01414b7f163f16778126f69b0ae505aa5c2c0b
DonFro <ayden.frometa@gmail.com>
2025-04-17 11:20:49 -0400

    fixed up the leaderboard and html for registration

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home-page'),
     path('leaderboard/', views.leaderboard_page, name='leaderboard-page'),
+    path('settings/', views.settings_page, name='settings-page'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from game.models import *
 
 
 # Create your views here.
+User = get_user_model()
 
 def home_page(request):
     return render(request, 'core/home.html')
 
 @login_required(login_url='login-page')
 def leaderboard_page(request):
-    return render(request, "core/leaderboard.html")
+    top_users = User.objects.order_by('-high_score')[:10]
+    return render(request, 'core/leaderboard.html', {'top_users': top_users})
+
+
+def settings_page(request):
+    return render(request, "core/settings.html")
