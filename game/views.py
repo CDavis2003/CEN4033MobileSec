2382fac5e25051ade279c9beeb20d6e216f4fa68
DonFro <ayden.frometa@gmail.com>
2025-04-22 10:46:49 -0400

    created login requirement for game

--- game/views.py
+++ game/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Game
 from questions.models import Question
 from .utils import pick_random_question, calculate_score
 
+@login_required(login_url='accounts:login-page')
 def play_game(request):
     user = request.user
 
